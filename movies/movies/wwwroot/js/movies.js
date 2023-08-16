f8d149b2fdf7e9fbad3ea1d089e203b173b39fac
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2023-08-16 18:21:20 +0300

    day num 4

---
+++ movies/movies/wwwroot/js/movies.js
@@ -0,0 +1,17 @@
+﻿function loadFile(event) {
+    $("#Poster-Contaner").removeClass('d-none');    
+    var reader = new FileReader();
+    reader.onload = function () {
+        var output = document.getElementById('output');
+        output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+};
+$("#year").datepicker({
+    format: 'yyyy',
+    viewMode: 'years',
+    minViewMode: 'years',
+    autoclose: true,
+    startDate: new Date('1950-01-01'),
+    endDate: new Date()
+});
